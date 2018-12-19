60d4c1f977149a55c67a1c152410431fd6e6ff1d
TretykovNick <nmtretyakov@mail.ru>
2018-12-19 20:45:36 +0300

    Added clear and operator= in array

---
+++ cmake-build-debug/CMakeFiles/Heaps.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Heaps.dir/heaps/array.cpp.obj"
+  "CMakeFiles/Heaps.dir/heaps/heap.cpp.obj"
+  "CMakeFiles/Heaps.dir/array.cpp.obj"
+  "CMakeFiles/Heaps.dir/binomial_heap.cpp.obj"
+  "CMakeFiles/Heaps.dir/heap2.cpp.obj"
+  "CMakeFiles/Heaps.dir/main.cpp.obj"
+  "Heaps.pdb"
+  "Heaps.exe"
+  "Heaps.exe.manifest"
+  "libHeaps.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Heaps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
